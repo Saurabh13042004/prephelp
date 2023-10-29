6de63af8b7769216eb2ae1bd399b39629c6ece76
Saurabh Shukla <99032013+Saurabh13042004@users.noreply.github.com>
2023-10-29 23:17:58 +0530

    admin-panel and loader added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ function Navbar() {
                     <div className="dropdown dropdown-end">
                         <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
                             <div className="w-10 rounded-full">
-                                <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                                <img src="https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg" />
                             </div>
                         </label>
                         <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,7 +1,44 @@
-import React from 'react'
 import { FaLink } from 'react-icons/fa'
 import { ImBin } from 'react-icons/im'
+import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import { db } from '../firebase';
+import { collection, doc, getDoc, getDocs, setDoc } from 'firebase/firestore';
+import Loader from '../components/Loader';
+
+
+
 function Admin() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const fetchData  = async ()=>{
+    setLoading(true);
+    try{
+      const response = await getDocs(collection(db,'formResponses'));
+      const data = [];
+      response.forEach((doc)=>{
+        data.push({id:doc.id,...doc.data()});
+      });
+      setPosts(data);
+      console.log(data);
+    } catch(error){
+      console.log(error);
+    }
+    setLoading(false);
+  }
+  const handleCheckboxChange = async (e, id) => {
+    const docRef = doc(db, 'formResponses', id);
+    const docSnap = await getDoc(docRef);
+    const data = docSnap.data();
+    const newData = { ...data, isApproved: !e.target.checked };
+    await setDoc(docRef, newData);
+    fetchData();
+  }
+  useEffect(()=>{
+    document.title = "Admin Panel";
+    fetchData();
+  },[]);
+
   return (
     <div>
     <div className="overflow-x-auto m-auto p-4">
@@ -17,47 +54,38 @@ function Admin() {
       </tr>
     </thead>
     <tbody>
-      {/* row 1 */}
-      <tr className=''>
+      { loading ? (<Loader/>) :  posts.map((entry)=>(
+      <tr className='' key={entry.id}>
 
-        <td>
-          <div className="flex items-center space-x-3">
-            <div className="avatar">
-              <div className="mask mask-squircle w-12 h-12">
-                <img src="https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg" alt="Avatar Tailwind CSS Component" />
-              </div>
-            </div>
-            <div>
-              <div className="font-bold">Aniket Kumar</div>
-              <div className="text-sm opacity-50">Google </div>
+      <td>
+        <div className="flex items-center space-x-3">
+          <div className="avatar">
+            <div className="mask mask-squircle w-12 h-12">
+              <img src="https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg" alt="Avatar " />
             </div>
           </div>
-        </td>
-        <td>
-        <FaLink/>
-    
-        </td>
-        <td>      <span className="badge badge-ghost badge-sm">Active</span></td>
-        <td>
-        <input type="checkbox" className="toggle"  />
-        </td>
-        <th>
-        <ImBin  size={19}/>
-        </th>
-      </tr>
+          <div>
+            <div className="font-bold">{entry.firstName} {entry.lastName}</div>
+            <div className="text-sm opacity-50">{entry.company} </div>
+          </div>
+        </div>
+      </td>
+      <td>
+      <FaLink/>
   
-    </tbody>
-    {/* foot */}
-    {/* <tfoot>
-      <tr>
-        <th></th>
-        <th>Name</th>
-        <th>Job</th>
-        <th>Favorite Color</th>
-        <th></th>
-      </tr>
-    </tfoot> */}
-    
+      </td>
+      <td>      <span className="badge badge-ghost badge-sm">{entry.isApproved ? 'Active' : 'Inactive'}</span></td>
+      <td>
+      <input type="checkbox" className="toggle" checked={entry.isApproved} onChange={(e) => handleCheckboxChange(e, entry.id)} />
+      </td>
+      <th>
+      <ImBin  size={19}/>
+      </th>
+    </tr>
+      ))}
+
+  
+    </tbody>    
   </table>
 </div>
     
