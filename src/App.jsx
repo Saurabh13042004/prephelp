b0716f50f09bf8527bd301e222447f02c92749a7
Saurabh Shukla <142835495+AyushSaxena0611@users.noreply.github.com>
2023-11-08 09:58:45 +0530

    login-added

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,17 @@ import Admin from './pages/Admin'
 import Form from './pages/Form'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Submitted from './pages/Submitted'
+import Login from './pages/Login'
 function App() {
 
   return (
     <Router>
-      <Navbar/>
+      {/* <Navbar/> */}
       <Routes>
         <Route path="/" element={<Form />} />
         <Route path="/admin" element={<Admin/>} />
         <Route path="/formSubmitted" element={<Submitted/>}/>
+        <Route path = "/login" element={<Login/>}/>
       </Routes>
     </Router>
   )
--- src/pages/Submitted.jsx
+++ src/pages/Submitted.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 function Submitted() {
   return (
-    <div>Submitted</div>
+    <>
+      <div className="hero min-h-screen" style={{ backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)' }}>
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">Thank You</h1>
+            <p className="mb-5">Your response has been successfully submitted! You will be mailed your reponse status soon. </p>
+            <button className="btn btn-primary">Go To Home</button>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
