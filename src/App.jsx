a4d77093e40ce3c31f4aa4fbf2a88f35bd75ec9e
Saurabh Shukla <99032013+Saurabh13042004@users.noreply.github.com>
2023-12-18 13:15:09 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@
 import './App.css'
 
 import Admin from './pages/Admin'
-import { useLocation } from 'react-router-dom';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Submitted from './pages/Submitted'
 import Login from './pages/Login'
@@ -15,19 +14,9 @@ import Error404 from './pages/Error404'
 import LandingPage from './components/LandingPage'
 import SignUp from './pages/SignUp'
 function App() {
-  const location = useLocation();
 
-  useEffect(() => {
-    require('preline/preline');
-  }, []);
-
-  useEffect(() => {
-    // @ts-ignore
-    HSStaticMethods.autoInit();
-  }, [location.pathname]);
   return (
     <Router>
-      
       {/* <Navbar/> */}
       <Routes>
         
